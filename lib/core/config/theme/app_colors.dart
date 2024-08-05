4ea3fe27344a558e61e50c92748d455220a40fd1
Ankush Prajapati <ankushpra49054@gmail.com>
2024-08-05 14:27:28 +0530

    App Colors added

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const primary=Color(0xff42CB3C);
+  static const lightbackground=Color.fromRGBO(242, 242, 242, 1);
+  static const darkbackground=Color(0xff0D0C0C);
+  static const grey=Color(0xffBEBEBE);
+  static const darkgrey=Color(0xff343434);
+}
